3078bb177ca32a090ccd41ff034bbbdc5bb8f100
navigamer123 <navi_gamer@abv.bg>
2023-09-11 01:20:03 +0300

    main

---
+++ include/main.h
@@ -0,0 +1,23 @@
+#include <Arduino_GFX_Library.h>
+#include "TAMC_GT911.h"
+#include "Arduino.h"
+#include "string.h"
+#include "SD.h"
+#include "FS.h"
+#include "Esp.h"
+#include "WiFi.h"
+
+#ifndef main_h
+#define main_h
+#define backround_collor WHITE
+#define SD_CS 10
+#define SPI_MOSI 11
+#define SPI_MISO 13
+#define SPI_SCK 12
+#define TFT_BL 2
+
+extern Arduino_RGB_Display *gfx;
+extern int network_Nubers;
+extern int mode;
+extern int network_Nubers;
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,165 @@
+#include <Arduino.h>
+#include "main.h"
+#include <lvgl.h>
+#include <demos/lv_demos.h>
+#include "touch.h"
+#include "sq_studio/ui.h"
+
+Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
+    41 /* DE */, 40 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
+    14 /* R0 */, 21 /* R1 */, 47 /* R2 */, 48 /* R3 */, 45 /* R4 */,
+    9 /* G0 */, 46 /* G1 */, 3 /* G2 */, 8 /* G3 */, 16 /* G4 */, 1 /* G5 */,
+    15 /* B0 */, 7 /* B1 */, 6 /* B2 */, 5 /* B3 */, 4 /* B4 */,
+    0 /* hsync_polarity */, 210 /* hsync_front_porch */, 30 /* hsync_pulse_width */, 16 /* hsync_back_porch */,
+    0 /* vsync_polarity */, 22 /* vsync_front_porch */, 13 /* vsync_pulse_width */, 10 /* vsync_back_porch */);
+
+Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
+    800 /* width */, 480 /* height */, rgbpanel);
+
+// function declarations here:
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p);
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data);
+
+void initDSP();
+void sd_card_setup();
+static uint32_t screenWidth = gfx->width();
+static uint32_t screenHeight = gfx->height();
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t *disp_draw_buf;
+static lv_disp_drv_t disp_drv;
+int network_Nubers;
+String info_str;
+void setup()
+{
+  Serial.begin(115200);
+  yield();
+  WiFi.mode(WIFI_MODE_STA);
+  yield();
+  
+  initDSP();
+  sd_card_setup();
+  
+  File Ver;
+  info_str = lv_label_get_text(ui_info1);
+  Ver = SD.open("/ver.txt","r");  
+  info_str.replace("%version",Ver.readString());
+  lv_label_set_text(ui_info1,info_str.c_str());
+  
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  lv_timer_handler();
+}
+
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
+  lv_disp_flush_ready(disp);
+}
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+  if (touch_has_signal())
+  {
+    if (touch_touched())
+    {
+      data->state = LV_INDEV_STATE_PR;
+
+      /*Set the coordinates*/
+      data->point.x = touch_last_x;
+      data->point.y = touch_last_y;
+    }
+    else if (touch_released())
+    {
+      data->state = LV_INDEV_STATE_REL;
+    }
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+}
+void sd_card_setup()
+{
+  pinMode(SD_CS, OUTPUT);
+  digitalWrite(SD_CS, HIGH);
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+  if (!SD.begin(SD_CS, SPI, 1000000))
+  {
+    Serial.println("SD Card Mount Failed");
+  }
+  uint8_t cardType = SD.cardType();
+  if (cardType == CARD_NONE)
+  {
+    Serial.println("No SD card attached");
+  }
+}
+void initDSP()
+{
+  gfx->begin();
+#ifdef TFT_BL
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+
+  ledcSetup(0, 300, 8);
+  ledcAttachPin(TFT_BL, 0);
+  ledcWrite(0, 150); /* Screen brightness can be modified by adjusting this parameter. (0-255) */
+
+#endif
+  gfx->fillScreen(RED);
+  gfx->fillScreen(GREEN);
+  gfx->fillScreen(BLUE);
+  gfx->fillScreen(BLACK);
+  lv_init();
+  // Init touch device
+  pinMode(TOUCH_GT911_RST, OUTPUT);
+  digitalWrite(TOUCH_GT911_RST, LOW);
+  delay(10);
+  digitalWrite(TOUCH_GT911_RST, HIGH);
+  delay(10);
+  touch_init();
+  //  touch.setTouch( calData );
+
+  screenWidth = gfx->width();
+  screenHeight = gfx->height();
+#ifdef ESP32
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 4, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+#else
+  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 4);
+#endif
+  if (!disp_draw_buf)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  }
+  else
+  {
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight / 4);
+
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize the (dummy) input device driver */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+    ui_init();
+    Serial.println("Setup done");
+  }
+}
