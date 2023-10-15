cfd2365442d3bc55eed0c7adfe43c9f2dcae836a
Navi-gamer <100336447+Navi-gamer@users.noreply.github.com>
2023-10-15 13:43:16 +0300

    window update

--- include/windows/window.h
+++ include/windows/window.h
@@ -3,6 +3,10 @@
 #include "main.h"
 #include "windows/toolbar/toolbar.h"
 #include "windows/window_content/window_content.h"
+#define minimum(a, b) (((a) < (b)) ? (a) : (b))
+#ifndef LV_ATTRIBUTE_MEM_ALIGN
+#define LV_ATTRIBUTE_MEM_ALIGN
+#endif
 class window
 {
 public:
@@ -10,10 +14,12 @@ public:
 
     lv_obj_t *img_obj = lv_img_create(ui_Screen1);
     lv_obj_t *window_box = lv_obj_create(ui_Screen1);
+
     int x = 0;
     int y = 50;
     int width = 800;
     int height = 480;
+    bool decoded;
     File icon1;
     class window_content
     {
--- src/windows/window.cpp
+++ src/windows/window.cpp
@@ -1,14 +1,23 @@
 #include "windows/window.h"
-
 window::window()
 {
-    lv_img_set_src(img_obj, "sd/icons/04.png");
+    lv_img_set_src(img_obj, "1.png");
     lv_obj_set_x(window_box, x);
     lv_obj_set_y(window_box, y);
-    lv_obj_set_width(window_box,width);
-    lv_obj_set_height(window_box,height);
-    lv_obj_set_style_bg_img_src(window_box,&img_obj,LV_PART_MAIN | LV_STATE_DEFAULT);
-    icon1 = SD.open("04.jpeg")
+    lv_obj_set_width(window_box, width);
+    lv_obj_set_height(window_box, height);
+    lv_obj_set_style_bg_img_src(window_box, &img_obj, LV_PART_MAIN | LV_STATE_DEFAULT);
+    decoded = JpegDec.decodeSdFile("/icons/1.jpeg");
+    
+    if (decoded)
+    {
+     
+    }
+    else
+    {
+        Serial.println("no\nno\n pls");
+    }
+
     //_ui_flag_modify(window_box,LV_OBJ_FLAG_HIDDEN,_UI_MODIFY_FLAG_ADD);
 }
 window::window_content window_content()
